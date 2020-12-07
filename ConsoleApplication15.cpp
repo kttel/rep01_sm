6649d5e117137766fa4c5b04177f70587541f9cf
elizabeth <63110747+kttel@users.noreply.github.com>
2020-12-07 13:24:56 +0200

    changes 07.12
    added 1 line (printf 'changes'), deleted 2 lines

--- ConsoleApplication15.cpp
+++ ConsoleApplication15.cpp
@@ -16,11 +16,10 @@ void main()
     scanf_s("%f", &m);
     printf("Введіть значення c: ");
     scanf_s("%f", &c);
-    printf("Введіть значення t: ");
-    scanf_s("%f", &t);
 
     a = pow((m * tan(t * M_PI / 180) + fabs(c * sin(t * M_PI / 180))), (1 / 3.)) + sin(92 * M_PI / 180);
 
+    printf("Зміна");
     printf("\n\t\t\tA");
     printf("\n\t\t%e\n", a);
     system("pause");
